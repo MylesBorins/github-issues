bd23b8ab896757282bb2d34ef571075459ed4afd
Myles Borins <myles.borins@gmail.com>
2015-08-12 22:45:35 -0700

    Get index linting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+var React = require('react');
 
-var React = require('react')
 var App = React.createClass({
-  getInitialState: function () { return { n: 0 } },
+  getInitialState: function () { return { n: 0 }; },
   render: function () {
     return <div>
       <h1>clicked {this.state.n} times</h1>
       <button onClick={this.handleClick}>click me!</button>
-    </div>
+    </div>;
   },
   handleClick: function () {
-    this.setState({ n: this.state.n + 1 })
+    this.setState({ n: this.state.n + 1 });
   }
-})
-React.render(<App />, document.querySelector('#content'))
+});
+
+React.render(<App />, document.querySelector('#content'));
