ad8ecc5a5e71737ddc7c56ccb0108b84bd5a65b4
Myles Borins <myles.borins@gmail.com>
2015-08-13 08:32:14 -0700

    Naive first pass at rendering github data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,62 @@
 var React = require('react');
 
-var App = React.createClass({
-  getInitialState: function () { return { n: 0 }; },
+var github = require('./github');
+
+var Issue = React.createClass({
   render: function () {
-    return <div>
-      <h1>clicked {this.state.n} times</h1>
-      <button onClick={this.handleClick}>click me!</button>
-    </div>;
-  },
-  handleClick: function () {
-    this.setState({ n: this.state.n + 1 });
+    return (
+      <div className='issue'>
+        <h2 className='issueAuthor'>
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
   }
 });
 
-React.render(<App />, document.querySelector('#content'));
+// tutorial2.js
+var IssueList = React.createClass({
+  render: function () {
+    var issueNodes = this.props.data.map(function (issue) {
+      return (
+        <Issue author={issue.user.login}>
+          {issue.title}
+        </Issue>
+      );
+    });
+    return (
+      <div className='issueList'>
+        {issueNodes}
+      </div>
+    );
+  }
+});
+
+var IssueView = React.createClass({
+  getInitialState: function () {
+    return {data: []}
+  },
+  componentDidMount: function () {
+    github.issues(this.props.repo, function (err, issues) {
+      if (err) {
+        return console.log(new Error(err));
+      }
+      console.log(issues);
+      this.setState({data: issues});
+    }.bind(this));
+  },
+  render: function() {
+    return (
+      <div className='issueBox'>
+        <h1>Issues</h1>
+        <IssueList data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+React.render(
+  <IssueView repo='npm/npm'/>,
+  document.getElementById('content')
+);
