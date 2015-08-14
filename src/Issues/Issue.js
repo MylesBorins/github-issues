1e8fe5a77a8861034bda3bb013b87c6f2b622d3b
Myles Borins <myles.borins@gmail.com>
2015-08-14 03:02:01 -0700

    Avatar now is a link

--- src/Issues/Issue.js
+++ src/Issues/Issue.js
@@ -17,7 +17,7 @@ var UserData = React.createClass({
         <h4 className='username'>
           <a href={this.props.userURL} >{'@' + this.props.username}</a>
         </h4>
-        <img className='avatar' src={this.props.avatarURL + '&s=75'} alt={'avatar of ' + this.props.username} ></img>
+        <a href={this.props.userURL}><img className='avatar' src={this.props.avatarURL + '&s=75'} alt={'avatar of ' + this.props.username} ></img></a>
       </div>
     );
   }
