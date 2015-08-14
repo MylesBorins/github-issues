8c14bf81934ff37574d8164bc3481860b35fada3
Myles Borins <myles.borins@gmail.com>
2015-08-14 04:35:13 -0700

    fix URL

--- src/Issues/Issue.js
+++ src/Issues/Issue.js
@@ -9,7 +9,7 @@ var Issue = React.createClass({
   render: function () {
     return (
       <div className='issue'>
-        <TopBar issueURL={this.props.issueURL} issueNumber={this.props.issueNumber}/>
+        <TopBar issueURL={this.props.url} issueNumber={this.props.issueNumber}/>
         <UserData avatarURL={this.props.user.avatar_url} userURL={this.props.user.html_url} username={this.props.user.login} />
         <Content url={this.props.url} title={this.props.title} labels={this.props.labels}>
           {this.props.children}
